fa8da16a6514db73ae95e934d546a169ed4d2b90
Vamshi <110325220+VamsiP19@users.noreply.github.com>
2024-07-29 00:04:24 +0530

    Create All Unique Permutations of an array.py

---
+++ All Unique Permutations of an array.py
@@ -0,0 +1,6 @@
+from itertools import permutations
+class Solution:
+    def uniquePerms(self, arr, n):
+        # code here 
+        answer=list(sorted(set(permutations(arr))))
+        return answer
