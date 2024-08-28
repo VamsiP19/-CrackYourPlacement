0e2967f2d636ce38729f7e74e9ec105be91830b4
Vamshi <110325220+VamsiP19@users.noreply.github.com>
2024-08-28 21:47:14 +0530

    Create 104. Maximum Depth of Binary Tree.java

---
+++ 104. Maximum Depth of Binary Tree.java
@@ -0,0 +1,9 @@
+class Solution {
+    int max = 0;
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+}
